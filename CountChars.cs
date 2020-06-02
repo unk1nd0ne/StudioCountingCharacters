813239508209cdcdbace08ab570dde109c857cbc
unk1nd0ne <z.neil.schaefer@gmail.com>
2020-06-02 17:23:39 -0500

    Add project files.

---
+++ CountChars.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StudioCountingCharacters
+{
+    class CountChars
+    {
+        public static Dictionary<char, int> GetCount(string myString)
+        {
+            Dictionary<char, int> charCount = new Dictionary<char, int>();
+            char[] charArray = myString.ToLower().ToCharArray();
+            foreach (char character in charArray)
+            {
+                if (charCount.ContainsKey(character))
+                {
+                    charCount[character]++;
+                }
+                else
+                {
+                    if (Char.IsLetter(character))
+                    {
+                        charCount.Add(character, 1);
+                    }
+                }
+            }
+
+            return charCount;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+
+namespace StudioCountingCharacters
+{
+    class Program
+    {
+        private static void Main()
+        {
+            //string myString = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc accumsan sem ut ligula scelerisque sollicitudin. Ut at sagittis augue. Praesent quis rhoncus justo. Aliquam erat volutpat. Donec sit amet suscipit metus, non lobortis massa. Vestibulum augue ex, dapibus ac suscipit vel, volutpat eget massa. Donec nec velit non ligula efficitur luctus.";
+            //Console.WriteLine("Enter a string:");
+            //string myString = Console.ReadLine();
+            string myString = System.IO.File.ReadAllText(@"../../../TextFile1.txt");
+            Console.WriteLine(myString);
+            Dictionary<char, int> charCount = CountChars.GetCount(myString);
+
+
+            foreach (char key in charCount.Keys)
+            {
+                Console.WriteLine($"{key}: {charCount[key]}");
+            }
+        }
+    }
+}
